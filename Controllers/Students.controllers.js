bbe1626616be6dd7869d0fcc0e38b2739d97f175
Fahad <159504813+FAHADibrhiam@users.noreply.github.com>
2025-02-09 05:13:43 +0300

    Format API responses to be standardized and easy to understand using JSend

--- Controllers/Students.controllers.js
+++ Controllers/Students.controllers.js
@@ -2,18 +2,20 @@ const Students = require("../model/StudentSchema");
 const Instructor = require("../model/InstructorSchema");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
+const httpStatusText = require('../utils/httpStatusText')
+
 
 const register = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(401).json(errors);
+    return res.status(400).json({ status: httpStatusText.FAIL, data: errors });
   }
   const { FirstName, LastName, email, password } = req.body;
   const oldUser =
     (await Students.findOne({ email: email })) ||
     (await Instructor.findOne({ email: email }));
   if (oldUser) {
-    return res.status(401).json("Email is invalid");
+    return res.status(400).json({ status: httpStatusText.FAIL, data: "Email is invalid" });
   }
   // password Hashing
   const passwordHashing = await bcrypt.hash(password, 10);
@@ -27,43 +29,52 @@ const register = async (req, res) => {
     Courses_Enrolled: null,
   });
   await newUser.save();
-  res.json(newUser);
+  res.status(201).json({ status: httpStatusText.SUCCESS, data: { NewAccount: newUser } });
 };
 
 const login = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(401).json(errors);
+    return res.status(400).json({ status: httpStatusText.FAIL, data: errors });
   }
   const { email, password } = req.body;
   const user = await Students.findOne({ email: email });
   if (!user) {
-    return res.status(401).json("Email or password is incorrect");
+    return res
+      .status(400)
+      .json({ status: httpStatusText.FAIL, data: "Email or password is incorrect" });
   }
   const matchedPassword = await bcrypt.compare(password, user.password);
   if (!matchedPassword) {
-    return res.status(401).json("Email or password is incorrect");
+    return res
+      .status(400)
+      .json({ status: httpStatusText.fail, data: "Email or password is incorrect" });
   }
-  res.status(201).json(user);
+  res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: { Login: `welcome ${user.FirstName} ${user.LastName}` },
+  });
 };
 
 const updateInfoAccount = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(401).json(errors);
+    return res.status(400).json({ status: httpStatusText.FAIL, data: errors });
   }
   const UpdateInfo = req.params.UserID;
   try {
     const user = await Students.findOne({ _id: UpdateInfo });
     if (!user) {
-      return res.json("user is invalid");
+      return res.status(400).json({ status: httpStatusText.FAIL, data: "user is invalid" });
     }
     await Students.updateOne({ _id: UpdateInfo }, { $set: { ...req.body } });
   } catch (error) {
-    console.log(error);
-    res.json("Try again");
+    res.status(401).json({ status: httpStatusText.ERROR, message: error.message });
   }
-  res.json("done");
+  res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: { updateInfoAccount: await Instructor.findById(UpdateInfo) },
+  });
 };
 
 const DeleteAccount = async (req, res) => {
@@ -71,14 +82,14 @@ const DeleteAccount = async (req, res) => {
   try {
     const user = await Students.findOne({ _id: UserID });
     if (!user) {
-      return res.json("user is invalid");
+      return res.status(400).json({ status: httpStatusText.FAIL, data: "user is invalid" });
     }
     await Students.deleteOne({ _id: UserID });
   } catch (error) {
     console.log(error);
-    res.json("Try again");
+    res.status(401).json({ status: httpStatusText.ERROR, message: error.message });
   }
-  return res.json("done");
+  res.status(201).json({ status: httpStatusText.SUCCESS, data: null });
 };
 
 module.exports = {
